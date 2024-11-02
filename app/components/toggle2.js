c1c9544020d7a43e62801a8b1f7b100f157ee8ed
pianoninjas25 <ymleonards@gmail.com>
2024-11-03 02:03:05 +0700

    Add Dropdown Menu

--- app/components/toggle2.js
+++ app/components/toggle2.js
@@ -1,49 +1,135 @@
 'use client'
 
+import { Button, Select } from 'antd';
 import { useState } from 'react';
 
 const LocationToggle = () => {
   const [toggle, setToggle] = useState('location');
+  const [region, setRegion] = useState('')
+  const regionList = [
+    { name: 'JBRO', value: 'JBRO'},
+    { name: 'KSMP', value: 'KSMP'},
+    { name: 'CJWJ', value: 'CJWJ'},
+    { name: 'ÉJBN', value: 'ÉJBN'},
+    { name: 'SMTR', value: 'SMTR'},
+  ]
 
   return (
-    <div className="fixed grid justify-center w-full mt-20 z-[9999]"> {/* Horizontal centering */}
-      <div className="relative flex gap-2 w-fit p-1 rounded-t-lg shadow-md backdrop-blur-sm border border-white bg-white/40">
+    <div className="fixed grid justify-center w-full mt-28 z-[9999]"> {/* Horizontal centering */}
+      <div className="relative flex gap-8 w-fit p-1 px-4 rounded-t-lg backdrop-blur-sm border-2 border-b-0 border-white bg-white/50">
         <div
-          className={`absolute top-0 left-0 w-1/2 h-full rounded-t-lg bg-amtblue/50 transition-all duration-300 ${
-            toggle === 'location' ? 'translate-x-0' : 'translate-x-full'
+          className={`absolute top-0 left-0 w-1/2 h-full bg-amtblue/50 transition-all duration-300 ${
+            toggle === 'location' ? 'rounded-tl-md translate-x-0' : 'rounded-tr-md translate-x-full'
           }`}
         />
         <button
           onClick={() => setToggle('location')}
-          className={`w-20 text-center text-sm z-10 font-semibold transition-colors duration-300 ${
-            toggle === 'location' ? 'text-white' : 'text-gray-500'
+          className={`w-20 text-center text-sm z-10 transition-colors duration-300 ${
+            toggle === 'location' ? 'text-white font-bold' : 'text-gray-500'
           }`}
         >
           Location
         </button>
         <button
           onClick={() => setToggle('address')}
-          className={`w-20 text-center text-sm z-10 font-semibold transition-colors duration-300 ${
-            toggle === 'address' ? 'text-white' : 'text-gray-500'
+          className={`w-20 text-center text-sm z-10 transition-colors duration-300 ${
+            toggle === 'address' ? 'text-white font-bold' : 'text-gray-500'
           }`}
         >
           Address
         </button>
       </div>
+      
 
-      {/* DROPDOWN */}
-      <div className='w-44 h-fit p-2 shadow-md rounded-b-md backdrop-blur-md border border-white bg-white/40'>
+
+
+      <div className='relative w-[227px] h-fit p-2 shadow-md rounded-b-md backdrop-blur-sm  border-2 border-t-0 border-white'>
+        
+        {/* FIND LOCATION */}
         {toggle === 'location' && (
           <div className='grid p-2'>
-            <p className='place-self-center w-fit px-4 py-1 rounded-md text-sm text-center text-white bg-amtorange'>Find Location </p>
+            <button className='place-self-center w-fit px-4 py-1 rounded-md text-sm text-center text-white bg-amtorange'>Find Location </button>
           </div>
         )}
+
+
+
+        {/* DROPDOWN */}
         {toggle === 'address' && (
-          <div className='grid p-2'>
-            <span>DROPDOWN1 </span>
-            <span>DROPDOWN2</span>
-            <span>DROPDOWN3</span>
-          </div>
+          <>
+            {/* SELECT PROVINCE */}
+            <div className='grid place-items-center'>
+              <h2 className='place-self-start p-1 px-6 text-xs font-bold text-amtblue'>Province</h2>
+              <Select
+                showSearch
+                allowClear
+                style={{
+                  width: '80%',
+                }}
+                placeholder="Select Province"
+                maxTagCount="responsive"
+                onChange={(e) => setRegion(e)}
+                // defaultValue={regionName}
+                options={regionList}
+              />
+            </div>
+
+            {/* SELECT CITY */}
+            <div className='grid place-items-center pt-2'>
+              <h2 className='place-self-start p-1 px-6 text-xs font-bold text-amtblue'>City</h2>
+              <Select
+                showSearch
+                allowClear
+                style={{
+                  width: '80%',
+                }}
+                placeholder="Select City"
+                maxTagCount="responsive"
+                onChange={(e) => setRegion(e)}
+                // defaultValue={regionName}
+                options={regionList}
+              />
+            </div>
+
+            {/* SELECT SUBDISTRICT */}
+            <div className='grid place-items-center pt-2'>
+              <h2 className='place-self-start p-1 px-6 text-xs font-bold text-amtblue'>Subdistrict</h2>
+              <Select
+                showSearch
+                allowClear
+                style={{
+                  width: '80%',
+                }}
+                placeholder="Select Subdistrict"
+                maxTagCount="responsive"
+                onChange={(e) => setRegion(e)}
+                // defaultValue={regionName}
+                options={regionList}
+              />
+            </div>
+
+            {/* SELECT WARD */}
+            <div className='grid place-items-center py-2'>
+              <h2 className='place-self-start p-1 px-6 text-xs font-bold text-amtblue'>Ward</h2>
+              <Select
+                showSearch
+                allowClear
+                style={{
+                  width: '80%',
+                }}
+                placeholder="Select Ward"
+                maxTagCount="responsive"
+                onChange={(e) => setRegion(e)}
+                // defaultValue={regionName}
+                options={regionList}
+              />
+            </div>
+
+            <div className='grid p-2 pt-4'>
+              <button className='place-self-center w-fit px-4 py-1 rounded-md text-sm text-center text-white bg-amtorange'>Check Coverage </button>
+            </div>
+          
+          </>
         )}
 
       </div>
