c876c5d73f7ff1ebf150b0c4859c9461c7cc32d4
pianoninjas25 <ymleonards@gmail.com>
2024-11-06 20:56:45 +0700

    Add GoogleMaps #5

--- app/main/coverage/page.js
+++ app/main/coverage/page.js
@@ -16,45 +16,10 @@ const CheckCoverage = () => {
 
 
   return (
-    <div className='flex flex-col font-quicksand'>
-      <div className='w-full h-full bg-gradient-to-b from-amtblue/80 via-amtblue to-black'>
-        
-        
-
-
-
-        {/* <div className='absolute top-6 right-6 flex gap-6 z-[999]'>
-          <Image 
-          src={`/unifiber-text.svg`} 
-          width={100} 
-          alt="unifiber" 
-          preview={false}
-          />
-          
-          <button 
-          onClick={() => signOut()} 
-          className='flex items-center justify-around gap-2 w-fit h-fit p-2 px-3 text-sm text-white rounded-md shadow-sm bg-amtorange/80 hover:bg-amtorange'
-          >
-          Logout 
-          <IoLogOut size={20} />
-          </button>
-
-          </div> */}
-
-
-          <div className=''>
-            {/* <h1 className='text-white'>MAP</h1> */}
-            <div className='w-full h-screen overflow-hidden'>
-              {/* {session && <MapContainer toggle={toggle} />} */}
-              <MapContainer />
-            </div>
-          </div>
-
-
-
-
+    <div className='w-full h-full bg-gradient-to-b from-amtblue/80 via-amtblue to-black'>
+      <div className='w-full h-screen overflow-hidden'>
+        <MapContainer />
       </div>
-      
     </div>
   )
 }
