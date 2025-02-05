705e447859e7be5772e66c9a0e6538806bab7129
pianoninjas25 <ymleonards@gmail.com>
2025-02-05 17:09:48 +0700

    Refactor after UAT-2

--- app/main/ticketing/page.js
+++ app/main/ticketing/page.js
@@ -6,7 +6,7 @@ const Ticketing = () => {
       <h1 className="text-sm font-semibold text-amtorange">Ticketing System</h1>
       <h1 className="my-4 text-xl font-semibold text-red-600">Under Development</h1>
       <p className="text-gray-700">
-        We're sorry, but this page is currently under development.<br />
+        We are sorry, but this page is currently under development.<br />
         Please contact the administrator if you have any questions.
       </p>
     </div>
